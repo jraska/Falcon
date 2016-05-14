42926bd2bfcf9b3cad8b3757509c5a2bdabda6a4
Josef Raska <josef.raska@gmail.com>
2016-05-14 15:14:20 +0100

    Add test on dialog showing with espresso

---
+++ falcon-sample/src/androidTest/java/com/jraska/falcon/sample/EspressoSpoonTest.java
@@ -0,0 +1,73 @@
+package com.jraska.falcon.sample;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import com.jraska.falcon.FalconSpoon;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoSpoonTest extends ActivityInstrumentationTestCase2<SampleActivity> {
+  public EspressoSpoonTest() {
+    super(SampleActivity.class);
+  }
+
+  @Before
+  public void setUp() {
+    injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    getActivity();
+  }
+
+  @Test
+  public void testDialogTakenInScreenshot() throws Exception {
+    File screenshotWithoutDialogFile = FalconSpoon.screenshot(getActivity(), "No_dialog");
+    double screenHsvValueWithoutDialog = computeAverageHsvValue(screenshotWithoutDialogFile);
+
+    onView(withId(R.id.show_dialog)).perform(click());
+    onView(withText("Screenshot")).check(matches(isDisplayed()));
+
+    File screenshotWithDialogFile = FalconSpoon.screenshot(getActivity(), "Dialog_test");
+    double screenHsvValueWithDialog = computeAverageHsvValue(screenshotWithDialogFile);
+
+    //HSV value fo screenshot without dialog should be much higher, due to dimming around dialog
+    double withoutScreenshotHsvRatio = screenHsvValueWithoutDialog / screenHsvValueWithDialog;
+    Assertions.assertThat(withoutScreenshotHsvRatio).isGreaterThan(1.5);
+  }
+
+  private double computeAverageHsvValue(File bitmapFile) {
+    BitmapFactory.Options sampleImageOptions = new BitmapFactory.Options();
+    sampleImageOptions.inSampleSize = 4;
+
+    Bitmap bitmap = BitmapFactory.decodeFile(bitmapFile.getAbsolutePath(), sampleImageOptions);
+    return computeAverageHsvValue(bitmap);
+  }
+
+  private double computeAverageHsvValue(Bitmap bitmap) {
+    int[] pixelColors = new int[bitmap.getWidth() * bitmap.getHeight()];
+    bitmap.getPixels(pixelColors, 0, bitmap.getWidth(), 0, 0,
+        bitmap.getWidth(), bitmap.getHeight());
+
+    double sum = 0;
+    float[] hsv = new float[3];
+    for (int pixelColor : pixelColors) {
+      Color.colorToHSV(pixelColor, hsv);
+
+      sum += hsv[2];
+    }
+
+    return sum / pixelColors.length;
+  }
+}
