9a3c40958c8117616b843d3373321a7c9462d3b9
Josef Raska <josef.raska@gmail.com>
2016-07-27 01:45:35 +0100

    Set CI_BUILD build config variable o check CI builds in runtime

--- falcon-sample/src/androidTest/java/com/jraska/falcon/sample/CICheck.java
+++ falcon-sample/src/androidTest/java/com/jraska/falcon/sample/CICheck.java
@@ -5,11 +5,11 @@ import org.junit.Assume;
 import static org.hamcrest.Matchers.is;
 
 public final class CICheck {
-  public static void assumeNoCI(){
+  public static void assumeNoCI() {
     Assume.assumeThat("Test should not run on CI server.", isContinuousIntegration(), is(false));
   }
 
   public static boolean isContinuousIntegration() {
-    return Boolean.parseBoolean(System.getenv("CI"));
+    return BuildConfig.CI_BUILD;
   }
 }
